c4c2d04b8d854026a38567c14b480271a4fa20e7
pedroyoung95 <jty9531@gmail.com>
2020-12-09 10:31:03 +0900

    12/9 10:30 수업내용

--- src/member/command/JoinHandler.java
+++ src/member/command/JoinHandler.java
@@ -1,13 +1,60 @@
 package member.command;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import member.service.JoinRequest;
 import mvc.command.CommandHandler;
 
 public class JoinHandler implements CommandHandler{
+	private static final String FORM_VIEW = "joinForm";
+	//private JoinService joinService = new JoinService();
+	
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		return "joinForm";
+		if(req.getMethod().equalsIgnoreCase("GET")) {
+			return processForm(req, res);
+		} else if(req.getMethod().equalsIgnoreCase("POST")) {
+			return processSubmit(req, res);
+		} else {
+			res.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+			return null;
+		}
+	}
+	
+	private String processForm(HttpServletRequest req, HttpServletResponse res) {
+		return FORM_VIEW;
+	}
+	
+	private String processSubmit(HttpServletRequest req, HttpServletResponse res) {
+		//JoinService에서 처리할 정보를 Bean클래스인 JoinRequest에 담는 과정
+		JoinRequest joinReq = new JoinRequest();
+		joinReq.setId(req.getParameter("id"));
+		joinReq.setName(req.getParameter("name"));
+		joinReq.setPassword(req.getParameter("password"));
+		joinReq.setConfirmPassword(req.getParameter("confirmPassword"));
+		
+		Map<String, Boolean> errors = new HashMap<>();
+		req.setAttribute("errors", errors);
+		
+		joinReq.validate(errors);
+		
+		if(!errors.isEmpty()) {
+			return FORM_VIEW;
+		}
+		
+//		try {
+//			joinService.join(joinReq);
+//			return "/WEB-INF/view/joinSucess.jsp";
+//		} catch (Exception e) {
+//			errors.put("duplicatedId", Boolean.TRUE);
+//			return FORM_VIEW;
+//			e.printStackTrace();
+//		}
+		
+		return null;
 	}
 }
