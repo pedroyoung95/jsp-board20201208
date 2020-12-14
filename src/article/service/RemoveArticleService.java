c2d58d8bf85dc67506f2bd098aab7b0f53ce0ba0
pedroyoung95 <jty9531@gmail.com>
2020-12-14 17:55:25 +0900

    12/14 17:55 수업내용

--- src/article/service/RemoveArticleService.java
+++ src/article/service/RemoveArticleService.java
@@ -1,5 +1,12 @@
 package article.service;
 
-public class RemoveArticleService {
+import article.dao.ArticleContentDao;
+import article.dao.ArticleDao;
 
+public class RemoveArticleService {
+	
+	private ArticleDao articleDao = new ArticleDao();
+	private ArticleContentDao contentDao = new ArticleContentDao();
+	
+	public void removeArticle
 }
